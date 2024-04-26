a33e413769723739aef8e89f0e16d97f310202cb
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-04-26 18:02:30 +0400

    commit
    fix p

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let question = Array.from(document.getElementsByClassName('question'));
 
-console.log(question);
+let rotatearw = Array.from(document.getElementsByClassName('arrow'));
 
 function toggleanswers(question) {
   question.addEventListener('click', function (event) {
-    let answer = event.target.nextElementSibling;
+    let answer = event.currentTarget.nextElementSibling;
     answer.classList.toggle('show_answr');
+    rotatearw.classList.toggle('rotate');
   });
 }
 
