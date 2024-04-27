33acc4209fbbc4e036e0dcb2e05c2e8f8573b6c7
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-04-27 23:04:09 +0400

    bsb
    sbgb

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 let question = Array.from(document.getElementsByClassName('question'));
 let arrows = Array.from(document.querySelectorAll('.arrow'));
 
-function toggleanswers(question, index) {
+function toggleanswers(question, arrows, index) {
   question.addEventListener('click', function (event) {
     let answer = event.currentTarget.nextElementSibling;
     answer.classList.toggle('diplayblock');
     let boldquestion = event.currentTarget;
     boldquestion.classList.toggle('boldquestion');
-    let arrow = arrows.forEach[index];
-    arrow.classList.toggle('rotate');
+    if (answer.classList.includes('diplayblock')) {
+      arrows[index].classList.add('rotate');
+    } else {
+      arrows[index].classList.remove('rotate');
+    }
   });
 }
-
 question.forEach(toggleanswers);
