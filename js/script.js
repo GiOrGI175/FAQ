e5be1b1b828824db4396bc9c557ac5d0628e10ab
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-04-27 15:38:15 +0400

    fix
    js code fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let question = Array.from(document.getElementsByClassName('question'));
 let arrows = Array.from(document.querySelectorAll('.arrow'));
 
-function toggleanswers(question) {
+function toggleanswers(question, index) {
   question.addEventListener('click', function (event) {
     let answer = event.currentTarget.nextElementSibling;
     answer.classList.toggle('diplayblock');
     let boldquestion = event.currentTarget;
     boldquestion.classList.toggle('boldquestion');
-    let arrow = arrows.forEach(index);
+    let arrow = arrows.forEach[index];
     arrow.classList.toggle('rotate');
   });
 }
